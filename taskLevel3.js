16c59bcdbe8a3d05531773fbffadf781adf0dbc4
Anton Minich <ant.minich@gmail.com>
2023-12-27 20:51:39 +0300

    add new file taskLevel3 and done several tasks

---
+++ taskLevel3.js
@@ -0,0 +1,60 @@
+// Дано некоторое число. Проверьте, что цифры этого числа расположены по возрастанию.
+
+let number = 12345;
+let arr = String(number).split('').map(Number);
+if (arr.sort((a,b) => a-b).join('') == number) {
+    console.log('Цифры числа расположены по возрастанию');
+} else {
+    console.log(false);
+}
+
+// Удалите из массива все пустые строки.
+arr = [1, '', '', 2, 3, '', 5];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length == 0) {
+        arr.splice(i,1);
+        i--;
+    }
+    
+}
+console.log(arr);
+
+// Отсортируйте элементы в каждом подмассиве.
+arr = [
+	[2, 1, 4, 3, 5],
+	[3, 5, 2, 4, 1],
+	[4, 3, 1, 5, 2],
+];
+for (let i = 0; i < arr.length; i++) {
+    arr[i].sort((a,b) => a - b);
+}
+console.log(arr);
+
+// Удалите из большего массива лишние элементы с конца так, чтобы длины массивов стали одинаковыми.
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3, 4, 5];
+for (let i = arr2.length; i > arr1.length; i--) {
+    arr2.splice(i-1,1);
+}
+console.log(arr2);
+
+// Выведите в консоль все числа в промежутке от 10 до 1000, у которых предпоследняя цифра четная.
+for (let i = 10; i <= 1000; i++) {
+    let string = String(i).split('').reverse();
+    if(string[1] % 2 == 0) {
+        console.log(Number(string[1]));
+    }
+}
+
+
+// Дан массив. Удалите из него каждый пятый элемент.
+arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+let count = 4;
+for (let i = count; i < arr.length; i+=count) {
+        arr.splice(i,1);
+}
+console.log(arr);
+
+
+
+
