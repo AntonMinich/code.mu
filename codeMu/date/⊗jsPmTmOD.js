046928919e35b426a31d49f8e0e39d981e9f5d39
Anton Minich <ant.minich@gmail.com>
2024-01-15 19:22:30 +0300

    add task

---
+++ codeMu/date/⊗jsPmTmOD.js
@@ -0,0 +1,11 @@
+// #1
+let date = new Date();
+console.log(date.getDate());
+
+// #2
+date = new Date();
+console.log(date.getMonth());
+
+// #3
+date = new Date();
+console.log(date.getFullYear());
