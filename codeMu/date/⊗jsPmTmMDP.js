2a82f08a01e33dd04e0d5f84e42c20c868774f0b
Anton Minich <ant.minich@gmail.com>
2024-01-17 20:11:23 +0300

    completed task

--- codeMu/date/⊗jsPmTmMDP.js
+++ codeMu/date/⊗jsPmTmMDP.js
@@ -5,3 +5,8 @@ let result = (day2 - day1) / (1000 * 60 * 60 * 24);
 console.log(result);
 
 // #2
+now = new Date();
+day1 = new Date(now.getFullYear(), now.getMonth(), 20);
+day2 = new Date(now.getFullYear(), now.getMonth() + 1, 10);
+result = (day2 - day1) / (1000 * 60 * 60 * 24);
+console.log(result);
