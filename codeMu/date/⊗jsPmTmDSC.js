60fe157ddf034d37e63433e302578e7a69ccb44c
Anton Minich <ant.minich@gmail.com>
2024-01-19 20:24:49 +0300

    completed task

---
+++ codeMu/date/⊗jsPmTmDSC.js
@@ -0,0 +1,8 @@
+// #1
+let date1 = '2020-11-31';
+let date2 = '2020-12-01';
+if (date2 > date1) {
+    console.log(date2 + ' больше ' + date1);
+} else {
+    console.log(date1 + ' больше ' + date2);
+}
