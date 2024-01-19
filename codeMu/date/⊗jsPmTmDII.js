0a131f5bb342d1a6f8677f6155ff03cf3287a952
Anton Minich <ant.minich@gmail.com>
2024-01-19 20:59:42 +0300

    completed task

---
+++ codeMu/date/⊗jsPmTmDII.js
@@ -0,0 +1,30 @@
+// #1
+let date = '06-02';
+
+if (date >= '03-21' && date <= '04-20') {
+    console.log('Овен');
+} else if (date >= '04-21' && date <= '05-20') {
+    console.log('Телец');
+} else if (date >= '05-21' && date <= '06-21') {
+    console.log('Близнецы');
+} else if (date >= '06-22' && date <= '07-22') {
+    console.log('Рак');
+} else if (date >= '07-23' && date <= '08-23') {
+    console.log('Лев');
+} else if (date >= '08-24' && date <= '09-25') {
+    console.log('Дева');
+} else if (date >= '09-24' && date <= '10-23') {
+    console.log('Весы');
+} else if (date >= '10-24' && date <= '11-22') {
+    console.log('Скорпион');
+} else if (date >= '11-23' && date <= '12-21') {
+    console.log('Стрелец');
+} else if (date >= '12-22' && date <= '01-20') {
+    console.log('Козерог');
+} else if (date >= '01-21' && date <= '02-20') {
+    console.log('Водолей');
+} else {
+    console.log('Рыбы');
+}
+
+   
