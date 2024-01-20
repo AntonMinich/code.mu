f0870fa1d7d55f73759176902bf8d99dc3ab8245
Anton Minich <ant.minich@gmail.com>
2024-01-20 15:03:54 +0300

    add task ⊗jsPmDmEGAS

---
+++ codeMu/DOM/index.js
@@ -0,0 +1,16 @@
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+console.log(elem1);
+console.log(elem2);
+console.log(elem3);
+
+
+let elem4 = document.querySelector('#block p');
+console.log(elem4);
+
+let elem5 = document.querySelector('.block p');
+console.log(elem5);
+
+let elem6 = document.querySelector('.www');
+console.log(elem6);
