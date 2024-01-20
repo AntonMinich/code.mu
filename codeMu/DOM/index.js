f40d69d72b52884b66a2ea3023212d798459ba92
Anton Minich <ant.minich@gmail.com>
2024-01-20 15:17:13 +0300

    completed task ⊗jsPmDmNEH

--- codeMu/DOM/index.js
+++ codeMu/DOM/index.js
@@ -1,36 +1,52 @@
-let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
-let elem3 = document.querySelector('#elem3');
-console.log(elem1);
-console.log(elem2);
-console.log(elem3);
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// console.log(elem1);
+// console.log(elem2);
+// console.log(elem3);
 
 
-let elem4 = document.querySelector('#block p');
-console.log(elem4);
+// let elem4 = document.querySelector('#block p');
+// console.log(elem4);
 
-let elem5 = document.querySelector('.block p');
-console.log(elem5);
+// let elem5 = document.querySelector('.block p');
+// console.log(elem5);
 
-let elem6 = document.querySelector('.www');
-console.log(elem6);
+// let elem6 = document.querySelector('.www');
+// console.log(elem6);
 
-let button = document.querySelector('#button');
-button.addEventListener('click', function(){
-    alert('!!!');
-})
+// let button = document.querySelector('#button');
+// button.addEventListener('click', func);
+// function func(){
+//     alert('!!!');
+// }
 
-let button1 = document.querySelector('#button1');
-button1.addEventListener('click', function() {
-    alert(1);
-})
 
-let button2 = document.querySelector('#button2');
-button2.addEventListener('click', function(){
-    alert (2);
-})
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function() {
+//     alert(1);
+// })
 
-let button3 = document.querySelector('#button3');
-button3.addEventListener('click', function(){
-    alert(3);
-})
+// let button2 = document.querySelector('#button2');
+// button2.addEventListener('click', function(){
+//     alert (2);
+// })
+
+// let button3 = document.querySelector('#button3');
+// button3.addEventListener('click', function(){
+//     alert(3);
+// })
+
+let button4 = document.querySelector('#button4');
+let button5 = document.querySelector('#button5');
+
+button4.addEventListener('click', func1);
+function func1() {
+	alert(1);
+}
+
+button5.addEventListener('click', func2);
+
+function func2() {
+	alert(2);
+}
