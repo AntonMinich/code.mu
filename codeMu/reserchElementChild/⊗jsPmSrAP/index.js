b8a03a1ef00e1cd0a99fa63bbdc21741bf20e5eb
Anton Minich <ant.minich@gmail.com>
2024-01-28 16:47:52 +0300

    completed task ⊗jsPmSrAP

---
+++ codeMu/reserchElementChild/⊗jsPmSrAP/index.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector('#elem');
+let parent = elem.closest('div');
+alert('result task 1 - ' + parent);
+
+
+let elem2 = document.querySelector('#elem2');
+let parent2 = elem2.closest('.www');
+alert('result task2 - ' + parent2);
