b70412c6d15a964c2c5a792d3ad3f34e5f914028
Anton Minich <ant.minich@gmail.com>
2024-01-28 17:14:08 +0300

    completed task ⊗jsPmSrIE

---
+++ codeMu/reserchElementChild/⊗jsPmSrIE/index.js
@@ -0,0 +1,5 @@
+let parent = document.querySelector('#parent');
+let elems1 = parent.getElementsByClassName('www');
+let elems2 = parent.getElementsByClassName('ggg');
+let result1 = [...elems1];
+console.log(result1);
