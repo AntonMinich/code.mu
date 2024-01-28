64ef974109c757a640ccca212701d95ad9969319
Anton Minich <ant.minich@gmail.com>
2024-01-28 11:07:47 +0300

    completed task ⊗jsPmAtStM

---
+++ codeMu/attributes/⊗jsPmAtStM/index.js
@@ -0,0 +1,6 @@
+let elem = document.querySelector('#elem');
+elem.setAttribute('value', 'атрибут установлен');
+
+
+let elem2 = document.querySelector('#elem2');
+elem2.setAttribute('class', 'valid');
