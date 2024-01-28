81a0e51a8d4256af622ecba3ffbfa7453215e2cc
Anton Minich <ant.minich@gmail.com>
2024-01-28 15:31:55 +0300

    completed task ⊗jsPmStSA

---
+++ codeMu/stylization/⊗jsPmStSA/index.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector('#elem');
+let button = document.querySelector('#button');
+button.addEventListener('click', function(){
+    elem.style.width = '400px';
+    elem.style.height = '400px';
+    elem.style.border = 'solid';
+
+})
