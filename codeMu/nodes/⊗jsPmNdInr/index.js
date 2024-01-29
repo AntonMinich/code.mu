ebca682f306c8672b7db078fcf5e94b420017243
Anton Minich <ant.minich@gmail.com>
2024-01-29 19:30:19 +0300

    completed task ⊗jsPmNdInr

---
+++ codeMu/nodes/⊗jsPmNdInr/index.js
@@ -0,0 +1,11 @@
+let elem = document.getElementById('elem');
+console.log(elem.lastChild);
+console.log(elem.lastElementChild);
+
+let elem2 = document.querySelector('#elem2');
+console.log(elem2.nextSibling);
+console.log(elem2.nextElementSibling);
+
+let elem3 = document.getElementById('elem3');
+console.log(elem3.previousSibling);
+console.log(elem3.previousElementSibling);
