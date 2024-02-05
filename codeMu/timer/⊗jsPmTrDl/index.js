549449bc9df15d26c7065626a4ca881e21ec2b1d
Anton Minich <ant.minich@gmail.com>
2024-02-05 20:44:12 +0300

    completed task

--- codeMu/timer/⊗jsPmTrDl/index.js
+++ codeMu/timer/⊗jsPmTrDl/index.js
@@ -2,3 +2,14 @@ let result = document.querySelector('#result');
 setTimeout(function(){
     result.textContent = 'Hello world!';
 },5000)
+
+
+let i = 0;
+function timer(){
+    setTimeout(function(){
+        console.log(++i);
+        timer();
+    }, (i+1) * 1000)
+}
+timer();
+
